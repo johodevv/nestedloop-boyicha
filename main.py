506e7eef8a7a3afabd324a882ff2ab85b95ed1af
johodevv <johodevv@gmail.com>
2026-04-30 23:09:12 +0500

    yangi masalalar yechdim

--- main.py
+++ main.py
@@ -6,3 +6,114 @@
 # for x in range(1,10+1):
 #     sum+=1/x
 # print(sum)
+
+
+# Geometrik shakl to'rt burchak
+
+# n=5
+# ssum=""
+# s="*"
+# for x in range (n):
+#     ssum+=s
+# for c in range(n):
+#     print(ssum)
+
+
+# sonlar juftligi
+
+# n=5
+
+# for x in range(0,n+1):
+#     ssum=""
+#     for j in range(0,n+1):
+#         ssum+=(f"({x}.{j})")
+#     print(ssum)
+
+
+# Sonlar juftligi da C xarfi
+
+# n=8
+
+# for x in range (n+1):
+#     ssum=""
+#     for j in range (n+1):
+#         if (x==0):
+#             ssum+=(f"({x}.{j})")
+#         if(x>0 and x<n):
+#             ssum+=(f"({x}.{j})")
+#             break
+#         if(x==n):
+#             ssum+=(f"({x}.{j})")
+#     print(ssum)
+
+
+# Sonlar juftligida ustunlar
+
+# n=10 # berilgan son      
+
+# for i in range(n):
+#     for j in range(n):
+#         if i==0 or i==9 or j%2== 0: 
+#             print("({}, {}) ".format(i,j),end="")
+#         else:
+#             print("       ", end="");
+#     print()    
+
+
+# sonlar royhati
+
+# n=5
+
+# for x in range(0,n+1):
+
+        
+#     ssum=""
+#     for j in range(0,n+1):
+#         if x==0:
+#             ssum+=(f"{j}  ")
+#         else:
+#             ssum+=(f"{x}{j} ")
+#     print(ssum)
+
+
+
+# diaganal
+
+# n=5
+
+
+# for x in range(n):
+#     diaganal=""
+#     for j in range(n):
+#         if x==j:
+#             diaganal+="*"
+#         else:
+#             diaganal+=" "
+#     print(diaganal)
+
+#sonlar diaganali 
+
+# n=6
+# for x in range(n+1):
+#     diaganal=""
+#     for j in range (n+1):
+#         if x==j:
+#             diaganal+=f"({x},{j})"
+#         else:
+#             diaganal+=" "
+#     print(diaganal)
+
+
+# teskari diaganal
+
+# n=6
+
+
+# for x in range(n,0,-1):
+#     diaganal=""
+#     for j in range (0,n):
+#         if x==j:
+#             diaganal+="*"
+#         else:
+#             diaganal+=" "
+#     print(diaganal)
